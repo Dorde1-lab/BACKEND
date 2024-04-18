9c10d9d5432ac8fa977f1b96ad48098d39eaeaa4
Dorde1-lab <djordjel4444@yahoo.com>
2024-04-18 18:49:50 +0200

    .

---
+++ ClassesOOP1/Models/Teacher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classesOOP.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string JobTitle { get; set; }
+
+        public string[] LearningModules { get; set; }
+
+        private string YearOfAward { get; set; };             // private (dostupan samo u ovoj klasi Teacher)...... dole primer zasto i kako moze postati public;
+
+
+        public void SayHello()
+        {
+            Console.WriteLine($"Hi, my name is {FirstName} {LastName} and i am {Age} years old.");
+        }
+
+
+
+
+
+
+
+        // metodom mozemo private prebaciti na public;
+        public void SetYearOfAward(string yearOfAward)
+        {
+            yearOfAward = yearOfAward;
+        }
+    }
+}
---
+++ ClassesOOP1/Program.cs
@@ -0,0 +1,53 @@
+﻿using classesOOP.Models;
+
+namespace classesOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  profesor 1
+
+            Teacher professor1 = new Teacher();
+
+            professor1.Age = 29;
+            professor1.FirstName = "Professor";
+            professor1.LastName = "Jedan";
+            professor1.Email = "professor@gmail.com";
+            professor1.LearningModules = new string[] { "C# Basic", " Advanced C#", "DataBase" };
+
+            professor1.SayHello();   // pozivanje metode iz konstruktora;
+
+            professor1.SetYearOfAward("2024");
+
+
+
+
+
+
+            //   profesor 2
+
+            Teacher professor2 = new Teacher();
+
+            professor2.Age = 44;
+            professor2.FirstName = "Profesor2";
+            professor2.LastName = "Dva";
+            professor2.Email = "professor2@gmail.com";
+            professor2.LearningModules = new string[] { "DataBase Development" };
+
+
+            Teacher Miroslav = new Teacher();
+            Miroslav.Age = 45;
+
+            Teacher Almir = new Teacher();
+            Almir.FirstName = "Almir";
+
+            Teacher professor5 = new Teacher();
+            Teacher professor6 = new Teacher();
+            Teacher professor7 = new Teacher();
+
+
+
+        }
+    }
+}
